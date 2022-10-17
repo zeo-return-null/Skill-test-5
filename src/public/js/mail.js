c0edb41351f51d457e092c0ae4c255f07980f1c8
Juan Sepulveda <cjfsepu@protonmail.com>
2022-10-16 22:02:19 -0300

    ✨ add: form to send mails

--- src/public/js/mail.js
+++ src/public/js/mail.js
@@ -1,9 +1,40 @@
-// Llamar a nuestra de /mail con fetch() 
+// Llamar a nuestra de /mail con fetch()
 
-const datos = await fetch("/mail")
+// const datos = await fetch("/mail")
 
-datos.map(dato => {
-    contenedor.innerHTML += `
-        <h3>Remitente: ${dato.envelope.from}<h3>
-    ` 
-})
+// datos.map(dato => {
+//     contenedor.innerHTML += `
+//         <h3>Remitente: ${dato.envelope.from}<h3>
+//     `
+// })
+
+// Obtener informacion del formulario
+const formMail = document.getElementById("mail-form")
+
+formMail.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const mailData = new FormData(e.target);
+  const newMail = {
+    from: mailData.get("from"),
+  to: mailData.get("to"),
+    subject: mailData.get("subject"),
+    text: mailData.get("text"),
+    html: mailData.get("text-html"),
+  };
+
+  // Post a la ruta de mail con los datos del formulario 
+  fetch("/mail", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newMail),
+  })
+    .then(() => {
+      e.target.reset();
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
